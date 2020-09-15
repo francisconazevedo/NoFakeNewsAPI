ae372563bff149ec6b63d45502d1fab67c353539
Francisco Azevedo <francisco.azevedo@cbm.se.gov.br>
2020-09-14 21:35:32 -0300

    Primeiro commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoFakeNews.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return null;
+        }
+    }
+}
